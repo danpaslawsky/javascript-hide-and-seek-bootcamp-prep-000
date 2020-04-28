90d4e107064e38423bae6b6563676094465fff82
danpaslawsky <danpaslawsky@gmail.com>
2020-04-28 17:58:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+ }
 
+function nestedTarget(){
+  return document.getElementById('nested').querySelectorAll('.target')[0];  
+ }
+
+function increaseRankBy(n){
+  const listArray = document.getElementById('app').querySelectorAll('.ranked-list li');
+   for (let i = 0; i < listArray.length; i++) {
+  listArray[i].innerHTML = (parseInt(listArray[i].innerHTML) + n).toString();
+ }
+
+}
+
+function deepestChild(){
+  
+ }
