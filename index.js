d1b22040812c11d943295e5c633e0e8d5c3e44a0
danpaslawsky <danpaslawsky@gmail.com>
2020-04-28 18:25:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
- return document.getElementById('grand-node').querySelectorAll
+ 
+  document.getElementById('grand-node').querySelectorAll
  }
