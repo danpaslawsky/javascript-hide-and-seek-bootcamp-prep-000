e6b4449e6e8d31a4ed707906654d0e6af767c5e9
danpaslawsky <danpaslawsky@gmail.com>
2020-04-28 19:23:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+ }
 
+function nestedTarget(){
+  return document.getElementById('nested').querySelectorAll('.target')[0];  
+ }
+
+function increaseRankBy(n){
+  const listArray = document.getElementById('app').querySelectorAll('.ranked-list li');
+   for (let i = 0; i < listArray.length; i++) {
+  listArray[i].innerHTML = (parseInt(listArray[i].innerHTML) + n);
+ }
+
+}
+
+function deepestChild(){
+ var nodes = document.querySelectorAll('#grand-node div');
+var mostDeep = nodes[nodes.length- 1];
+ return mostDeep;
+ }
+ 
